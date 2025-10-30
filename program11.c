712bff15d7b696eb8654694cbc9b95d23054d9a9
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-10-30 10:57:19 +0530

    Clean up comments and improve code readability
    Removed commented algorithm and program sections for clarity.

--- program11.c
+++ program11.c
@@ -1,18 +1,4 @@
-// ALGORITHM:
 
-// 1. For all 4-tuples do 
-//    1.1 get next 4-tuple
-//    1.2 If the operator is "Label" then 
-//         1.2.1 free the constant table
-//         1.2.2 write the 4-tuple
-//  1.3 If either or both of the operands are in the constant table then
-//        1.3.1 substitute the constant value(s) for the operand(s) 
-//        1.3.2 if either of the operands are intermediate results then  
-//                1.3.2.1 remove those operand(s)
-// from the constant table.1.4 if 4 - tuple operand(s)
-// are constant then 1.4.1 if operator is "JPCT" then 1.4.1.1 if the operand is true then 1.4.1.1.1 replace the JPCT with JP 1.4.1.1.2 write the new 4 - tuple else 1.4.1.1.3 eliminate the 4 - tuple 1.4.2 if operator is "=" then 1.4.2.1 place the variable and constant in the constant table 1.4.2.2 write the 4 - tuple with the constant operand 1.4.3 else 1.4.3.1 Perform the operation.1.4.3.2 Place the intermediate result name and constant value in the constant table.Else 1.4.4 write the 4 - tuple Consider the following sequence.
-
-//     PROGRAM :
 
 #include <stdio.h>
 #include <string.h>
@@ -56,7 +42,7 @@ void constant()
     char op, res1[5];
     for (i = 0; i < n; i++)
     {
-        if (isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0]) || strcmp(arr[i].op, "=") == 0) /*if both digits, store them in variables*/
+        if (isdigit(arr[i].op1[0]) && isdigit(arr[i].op2[0]) || strcmp(arr[i].op, "=") == 0) 
         {
             op1 = atoi(arr[i].op1);
             op2 = atoi(arr[i].op2);
@@ -80,7 +66,7 @@ void constant()
                 break;
             }
             sprintf(res1, "%d", res);
-            arr[i].flag = 1; /*eliminate expr and replace any operand below that uses result of this expr */
+            arr[i].flag = 1; 
             change(i, res1);
         }
     }
